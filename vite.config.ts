de866dfa334956cedb4b34a35a19cd0730ddec3a
patrykbernat17-spec <patrykbernat17@gmail.com>
2026-02-18 13:49:28 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/fluxsite.pl/", // TO JEST KLUCZOWE
+  base: "/",
   server: {
     host: "::",
     port: 8080,
